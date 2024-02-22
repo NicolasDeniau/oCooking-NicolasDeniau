5a7c4a94a1141ddc2f1f32c130ff86555cb242f2
NicolasDeniau <nicolas.deniau@oclock.school>
2024-02-22 12:01:20 +0100

    ajout affichage conditionnel dans la navbar du fullName

--- frontend/src/components/layouts/Navigation.tsx
+++ frontend/src/components/layouts/Navigation.tsx
@@ -3,34 +3,50 @@ import "../../assets/styles/navigation.scss";
 
 const Navigation = () => {
     // isAuth = false par défaut
-  const [isAuth, SetIsAuth] = useState(false);
+    const [isAuth, setIsAuth] = useState(false);
+    const [fullName, setFullName] = useState<string>();
 
-  return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Toutes les recettes</a>
-        </li>
-        <li>
-          <a href="/">A propos</a>
-        </li>
-        {isAuth ? (
-          <li>
-            <a href="/">Déconnexion</a>
-          </li>
-        ) : (
-          <li>
-            <a href="/">Connexion</a>
-          </li>
-        )}
-    </ul>
-      {/* on veut modifier la valeur du state au clic sur le bouton Se connecter */}
-      <button onClick={() => SetIsAuth(!isAuth)}>
-      {/* si isAuth = true, alors, on affiche Se déconnecter, sinon, on affiche Se connecter */}
-      {isAuth ? 'Se déconnecter' : 'Se connecter'}
-      </button>
-    </nav>
-  );
+    const handleClick = () => {
+        setIsAuth(!isAuth);
+
+        if (!isAuth) {
+            setFullName("Nicolas Deniau");
+        } else {
+        setFullName(undefined);
+        }
+    }
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Toutes les recettes</a>
+                </li>
+                <li>
+                    <a href="/">A propos</a>
+                </li>
+                {isAuth && fullName !== undefined && (
+                    <li>
+                        <a href="/">Profil: {fullName}</a>
+                    </li>
+                )}
+                {isAuth ? (
+                    <li>
+                        <a href="/">Déconnexion</a>
+                    </li>
+                ) : (
+                    <li>
+                        <a href="/">Connexion</a>
+                    </li>
+                )}
+            </ul>
+            {/* on veut modifier la valeur du state au clic sur le bouton Se connecter */}
+            <button onClick={() => handleClick()}>
+                {/* si isAuth = true, alors, on affiche Se déconnecter, sinon, on affiche Se connecter */}
+                {isAuth ? 'Se déconnecter' : 'Se connecter'}
+            </button>
+        </nav>
+    );
 };
 
 export default Navigation;
