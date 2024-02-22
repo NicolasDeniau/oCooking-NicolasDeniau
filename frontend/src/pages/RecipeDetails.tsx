4d88ab0ec1f788824ea26980bce795e5347736a9
NicolasDeniau <nicolas.deniau@oclock.school>
2024-02-22 13:28:36 +0100

    découverte useParams

--- frontend/src/pages/RecipeDetails.tsx
+++ frontend/src/pages/RecipeDetails.tsx
@@ -1,9 +1,15 @@
+import { useParams } from "react-router-dom";
+
 const RecipeDetails = () => {
+    const {id} = useParams();
+
+    console.log(id);
+
     return (
         <>
-            <h1>Recette.... TODO</h1>
+            <h1>Recette {id}</h1>
         </>
     )
-}
+};
 
 export default RecipeDetails;
